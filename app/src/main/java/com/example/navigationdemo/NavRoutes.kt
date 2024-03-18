a02f8cec4cdbfadacde2421ca49ab648a60bb4de
andersbor <anbo@zealand.dk>
2024-03-18 12:47:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/navigationdemo/NavRoutes.kt
@@ -0,0 +1,8 @@
+package com.example.navigationdemo
+
+sealed class NavRoutes(val route: String) {
+    object Home : NavRoutes("home")
+    object Welcome : NavRoutes("welcome")
+    object Profile : NavRoutes("profile")
+
+}
